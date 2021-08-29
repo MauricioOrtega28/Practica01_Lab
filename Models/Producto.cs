684a61b041f4663f1e009cb6f37a5f53c7c6d6a6
Mauricio Ortega <mauricio_ortega@usmp.pe>
2021-08-29 14:50:22 -0500

    creacion del model

---
+++ Models/Producto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Practica01_Lab.Models
+{
+    [Table("t_producto")]
+    public class Producto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("categoria")]
+        public string categoria { get; set; }
+
+        [Column("precio")]
+        public string precio { get; set; }
+        
+        [Column("descuento")]
+        public string descuento { get; set; }
+    }
+}
